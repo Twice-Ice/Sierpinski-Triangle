63f34984245cecd07c656139dea0376ecd787b1e
Twice_Ice <125768114+Twice-Ice@users.noreply.github.com>
2024-02-05 12:51:55 -0700

    proper scaling works now
    I changed a bunch of stuff, and now the triangle scales without a whole lot of lag. It's pretty efficient. However, the camera position isn't locked to a point on the triangle which is annoying.

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ screenX = 1920
 screenY = 1080
 screen = pygame.display.set_mode((screenX, screenY))
 doExit = False
+lines = 0
+cameraX = 0
+cameraY = 0
+scale = 1
+Clock = pygame.time.Clock()
 
 # https://docs.google.com/presentation/d/1_9vaR8Gl3kL8IgQ9PZxdM65N26y-_QPE6OMWVh0LTdU/edit#slide=id.g2675e1715c5_0_20
 
@@ -20,8 +25,8 @@ def onScreen(x, y):
 
 def sierpinski(x1, y1, x2, y2, x3, y3, counter, isEven, screen, scale):
     counter += 1
-    if counter <= math.floor(8 * scale):
-        if isEven and counter == math.floor(8 * scale): #only draws the triangles on the 8th frame to prevent overdraw.
+    if counter <= countScaleVal:
+        if isEven and counter == countScaleVal: #only draws the triangles on the 8th frame to prevent overdraw.
             # r = random.randint(0, 255)#(counter * 100) % 255
             # g = random.randint(0, 255)
             # b = random.randint(0, 255)
@@ -30,33 +35,38 @@ def sierpinski(x1, y1, x2, y2, x3, y3, counter, isEven, screen, scale):
             color = (baseColor, baseColor, baseColor)
             
             # pygame.draw.polygon(screen, color, ((x1, y1), (x2, y2), (x3, y3)))
-            if onScreen(x1, y1) and onScreen(x2, y2):
+            if onScreen(x1, y1) or onScreen(x2, y2):
                 pygame.draw.line(screen, color, (x1, y1), (x2, y2))
-            if onScreen(x2, y2) and onScreen(x3, y3):
+                global lines
+                lines += 1
+            if onScreen(x2, y2) or onScreen(x3, y3):
                 pygame.draw.line(screen, color, (x2, y2), (x3, y3))
-            if onScreen(x3, y3) and onScreen(x1, y1):
+                lines += 1
+            if onScreen(x3, y3) or onScreen(x1, y1):
                 pygame.draw.line(screen, color, (x3, y3), (x1, y1))
-        else:
-            pass #no point in drawing something that won't show up so just don't draw it.
+                lines += 1
 
-        # if onScreen(x1, y1) and onScreen(midPoint(x1, x2), midPoint(y1, y2)) and onScreen(midPoint(x3, x1), midPoint(y3, y1)):
-        sierpinski(x1, y1, midPoint(x1, x2), midPoint(y1, y2), midPoint(x3, x1), midPoint(y3, y1), counter, isEven, screen, scale)
-        # if onScreen(midPoint(x1, x2), midPoint(y1, y2)) and onScreen(x2, y2) and onScreen(midPoint(x3, x2), midPoint(y3, y2)):
-        sierpinski(midPoint(x1, x2), midPoint(y1, y2), x2, y2, midPoint(x3, x2), midPoint(y3, y2), counter, isEven, screen, scale)
-        # if onScreen(midPoint(x1, x3), midPoint(y1, y3)) and onScreen(midPoint(x3, x2), midPoint(y3, y2)) and onScreen(x3, y3):
-        sierpinski(midPoint(x1, x3), midPoint(y1, y3), midPoint(x3, x2), midPoint(y3, y2), x3, y3, counter, isEven, screen, scale)
+        midx1x2 = midPoint(x1, x2)
+        midy1y2 = midPoint(y1, y2)
+        midx3x1 = midPoint(x3, x1)
+        midy3y1 = midPoint(y3, y1)
+        midx3x2 = midPoint(x3, x2)
+        midy3y2 = midPoint(y3, y2)
+        if onScreen(x1, y1) or onScreen(midx1x2, midy1y2) or onScreen(midx3x1, midy3y1) and counter > 2:
+            sierpinski(x1, y1, midx1x2, midy1y2, midx3x1, midy3y1, counter, isEven, screen, scale)
+        if onScreen(midx1x2, midy1y2) or onScreen(x2, y2) or onScreen(midx3x2, midy3y2):
+            sierpinski(midx1x2, midy1y2, x2, y2, midx3x2, midy3y2, counter, isEven, screen, scale)
+        if onScreen(midx3x1, midy3y1) or onScreen(midx3x2, midy3y2) or onScreen(x3, y3):
+            sierpinski(midx3x1, midy3y1, midx3x2, midy3y2, x3, y3, counter, isEven, screen, scale)
         # pygame.display.flip()
         isEven *= -1
         return 0
 
-cameraX = 0
-cameraY = 0
-scale = 1
+
 
 # sierpinski(screenX/2, 0, 0, screenY, screenX, screenY, 0, True, screen, scale)
 
-moveSpeed = -5
-scaleSpeed = .05
+countScaleVal = 8 #math.floor(8 * ((scale - 1) / 8 + 1))
 while not doExit:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
@@ -65,22 +75,61 @@ while not doExit:
     # cameraX += 1
     # scale += .01
 
+    
+
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_LSHIFT]:
+        moveSpeed = -5 * (1/(scale + scale/100)) * 5
+    else:
+        moveSpeed = -5 * (1/(scale + scale/100))
+    if keys[pygame.K_LCTRL]:
+        scaleSpeed = (scale/100) * 2
+    else:
+        scaleSpeed = scale/100
     if keys[pygame.K_w]:
         cameraY -= moveSpeed
+        print("lines = " + str(lines))
     if keys[pygame.K_a]:
         cameraX -= moveSpeed
+        print("lines = " + str(lines))
     if keys[pygame.K_s]:
         cameraY += moveSpeed
+        print("lines = " + str(lines))
     if keys[pygame.K_d]:
         cameraX += moveSpeed
+        print("lines = " + str(lines))
+    if keys[pygame.K_r]:
+        cameraX = 0
+        cameraY = 0
+        scale = 1
+        countScaleVal = 8
     if keys[pygame.K_UP]:
         scale += scaleSpeed
+        # cameraX += moveSpeed
+        # cameraY += moveSpeed
+        if posx3 / (2 ** countScaleVal) > 7.5 and posx3 / (2 ** (countScaleVal + 1)) > 4.5:
+            countScaleVal += 1
+        print("lines = " + str(lines), scale, countScaleVal, posx3 / (2 ** countScaleVal))
     if keys[pygame.K_DOWN]:
         scale -= scaleSpeed
-    
+        # cameraX += moveSpeed
+        # cameraY += moveSpeed
+        if posx3 / (2 ** countScaleVal) < 4.5:
+            countScaleVal -= 1
+        print("lines = " + str(lines), scale, countScaleVal, posx3 / (2 ** countScaleVal))
+
+    lines = 0
+
     screen.fill((0, 0, 0))
-    sierpinski((screenX/2 + cameraX) * scale, (0 + cameraY) * scale, (0 + cameraX) * scale, (screenY + cameraY) * scale, (screenX + cameraX) * scale, (screenY + cameraY) * scale, 0, True, screen, scale)
+    posx1 = (screenX/2 + cameraX) * scale
+    posy1 = (0 + cameraY) * scale
+    posx2 = (0 + cameraX) * scale# - ((screenY) * scale) - screenY
+    posy2 = (screenY + cameraY) * scale
+    posx3 = (screenX + cameraX) * scale
+    posy3 = (screenY + cameraY) * scale
+
+
+    sierpinski(posx1, posy1, posx2, posy2, posx3, posy3, 0, True, screen, scale)
 
     pygame.display.flip()
 pygame.quit()
